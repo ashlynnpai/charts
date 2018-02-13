1730fd163531b0ba9df11c5a01ab11f46e7cdc77
ashlynnpai <ashlynnpai@gmail.com>
2018-02-13 11:39:59 -0500

    add tooltip

--- cycling-scatter/chart.js
+++ cycling-scatter/chart.js
@@ -2,99 +2,74 @@
 //https://bl.ocks.org/d3noob/6f082f0e3b820b6bf68b78f2f7786084
 // d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function(response) {
 //   var data = response.data;
-var data = [{
-    "Time": "36:50",
-    "Place": 1,
-    "Seconds": 2210,
-    "Name": "Marco Pantani",
-    "Year": 1995,
-    "Nationality": "ITA",
-    "Doping": "Alleged drug use during 1995 due to high hematocrit levels",
-    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
-  },
-  {
-    "Time": "36:55",
-    "Place": 2,
-    "Seconds": 2215,
-    "Name": "Marco Pantani",
-    "Year": 1997,
-    "Nationality": "ITA",
-    "Doping": "Alleged drug use during 1997 due to high hermatocrit levels",
-    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
-  },
-  {
-    "Time": "37:15",
-    "Place": 3,
-    "Seconds": 2235,
-    "Name": "Marco Pantani",
-    "Year": 1994,
-    "Nationality": "ITA",
-    "Doping": "Alleged drug use during 1994 due to high hermatocrit levels",
-    "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use"
-  },
-  {
-    "Time": "37:36",
-    "Place": 4,
-    "Seconds": 2256,
-    "Name": "Lance Armstrong",
-    "Year": 2004,
-    "Nationality": "USA",
-    "Doping": "2004 Tour de France title stripped by UCI in 2012",
-    "URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations"
-  },
-  {
-    "Time": "37:42",
-    "Place": 5,
-    "Seconds": 2262,
-    "Name": "Jan Ullrich",
-    "Year": 1997,
-    "Nationality": "GER",
-    "Doping": "Confessed later in his career to doping",
-    "URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case"
-  }];
+var data =
+
+    []
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var div = d3.select('.chart').append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+    // set the ranges
+    var parseTime = d3.timeParse("%M:%S");
+    var formatTime = d3.timeFormat("%M:%S");
+    var times = data.map(function(d) {
+      d.Time = parseTime(d.Time);
+      return d.Time;
+      })
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var svg = d3.select(".chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
 
 
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+      // Scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.Time; }));
+      y.domain([d3.max(data, function(d) { return d.Place; }),0]);
 
 
-  // set the ranges
-  var parseTime = d3.timeParse("%M:%S");
-  var times = data.map(function(d) {
-    d.Time = parseTime(d.Time);
-    return d.Time;
-    })
-  var x = d3.scaleTime().range([0, width]);
-  var y = d3.scaleLinear().range([height, 0]);
+      svg.selectAll("dot")
+          .data(data)
+          .enter().append("circle")
+          .attr("r", 5)
+          .attr("cx", function(d) { return x(d.Time); })
+          .attr("cy", function(d) { return y(d.Place); })
+          .on("mouseover", function(d) {
+              div.transition()
+               .duration(200)
+               .style("opacity", .9);
+              div.html(d.Name + "<br/>" + "Time: " + formatTime(d.Time) +
+               "<br/>" + "Place: " + d.Place +
+               "<br/>" + "Year: " + d.Year +
+               "<br/>" + "Nationality: " + d.Nationality +
+               "<br/>" + "Allegation: " + d.Doping
+               )
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+             })
+          .on("mouseout", function(d) {
+              div.transition()
+               .duration(500)
+               .style("opacity", 0)
+           });
 
-  var svg = d3.select(".chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
 
+      // Add the X Axis
+      var xAxis = d3.axisBottom(x)
+          .tickFormat(d3.timeFormat("%M:%S"));
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.Time; }));
-    y.domain([d3.max(data, function(d) { return d.Place; }),0]);
-
-
-    svg.selectAll("dot")
-        .data(data)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("cx", function(d) { return x(d.Time); })
-        .attr("cy", function(d) { return y(d.Place); });
-
-    // Add the X Axis
-    var xAxis = d3.axisBottom(x)
-        .tickFormat(d3.timeFormat("%M:%S"));
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the Y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+      // Add the Y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y));
