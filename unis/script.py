0f9a0e6781ce811111711a6ad0abf75439e9edc3
ashlynnpai <ashlynnpai@gmail.com>
2018-02-13 19:01:16 -0500

    remove python unicode prefix from data

--- unis/script.py
+++ unis/script.py
@@ -7,8 +7,8 @@ infodict = {}
 
 for row in range(4, 54):
     num = 67
-    infodict["uni"] = sheet['A' + str(row)].value
-    infodict["state"] = sheet['B' + str(row)].value
+    infodict["uni"] = str(sheet['A' + str(row)].value)
+    infodict["state"] = str(sheet['B' + str(row)].value)
     for i in range(2007,2018):
         infodict[i] = int(sheet[chr(num) + str(row)].value)
         num += 1
