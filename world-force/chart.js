baed869cbfa8bea5ac6edd0a9fdc5d1a4218be86
ashlynnpai <ashlynnpai@gmail.com>
2018-02-15 12:19:22 -0500

    replace circles with image

--- world-force/chart.js
+++ world-force/chart.js
@@ -20,14 +20,16 @@ d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countri
         .force("center_force", d3.forceCenter(width / 2, height / 2))
         .force("link", d3.forceLink(links).distance(20));
 
+
     var node = svg.append("g")
         .attr("class", "nodes")
-        .selectAll("circle")
+        .selectAll("image")
         .data(nodes)
         .enter()
-        .append("circle")
-        .attr("r", 5)
-        .attr("fill", "red");
+        .append("image")
+        .attr("xlink:href", "https://www.ashlynnpai.com/assets/flags.png")
+        .attr("width", 30)
+        .attr("height", 30);
 
     var link = svg.append("g")
         .attr("class", "links")
@@ -38,8 +40,8 @@ d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countri
         .attr("stroke-width", 1);
 
     simulation.on("tick", function() {
-        node.attr("cx", function(d) {return d.x;})
-        .attr("cy", function(d) {return d.y;});
+        node.attr("x", function(d) {return d.x;})
+        .attr("y", function(d) {return d.y;});
         link.attr("x1", function(d) {return d.source.x;})
         .attr("y1", function(d) {return d.source.y;})
         .attr("x2", function(d) {return d.target.x;})
